5365ba0716b1d752938783ed91ee703810afce38
Carifio24 <carifio24@gmail.com>
2024-06-12 15:08:29 -0400

    More work on non-glue line fit functionality.

--- src/cosmicds/line_fit_handler.py
+++ src/cosmicds/line_fit_handler.py
@@ -1,5 +1,6 @@
 from numpy import isnan
 from numpy.linalg import LinAlgError
+from solara import Reactive
 
 from cosmicds.utils import fit_line, line_mark
 
@@ -9,16 +10,19 @@ class LineFitHandler:
     def __init__(self, figure, **kwargs):
         self.lines = []
         self.slopes = []
-        self.active = False
+        self.active = Reactive(False)
         self.figure = figure
-        self._show_layers = kwargs.get("show_labels", True)
+        self._show_labels = kwargs.get("show_labels", True)
 
     def activate(self):
-        if self.active:
-            self._fit_to_traces()
-        else:
+        if self.active.value:
             self._clear_lines()
-        self.active = not self.active
+        else:
+            self._fit_to_traces()
+        self.active.set(not self.active.value)
+        print("end of activate")
+        print(self.figure.data)
+        print(id(self.figure))
         
     # The label displayed for each line
 
@@ -27,7 +31,7 @@ class LineFitHandler:
         return f"Slope: {slope}" if not isnan(slope) else None
 
     def _refresh_if_active(self):
-        if self.active:
+        if self.active.value:
             self._fit_to_traces()
 
     def _fit_line(self, trace):
@@ -35,7 +39,7 @@ class LineFitHandler:
 
     @property
     def x_range(self):
-        return self.figure.xaxis.range
+        return self.figure.layout.xaxis.range
 
     @property
     def show_labels(self):
@@ -61,13 +65,14 @@ class LineFitHandler:
         # Create the fit line object
         # Keep track of this line and its slope
         # For now, the line spans from 0 to twice the edge of the viewer
-        y = fit(self.x_range)
-        start_x, end_x = self.x_range
-        start_y, end_y = y
+        xrange = self.x_range
+        if xrange is None:
+            xrange = [0, 2]
+        y = fit(xrange)
         slope = fit.slope.value
         label = self.label(trace, fit) if self.show_labels else None
         color = self._get_layer_color(trace)
-        line = line_mark(start_x, start_y, end_x, end_y, color, label)
+        line = line_mark(xrange[0], y, xrange[1], y, color, label)
         return line, slope
 
     def _fit_to_trace(self, trace, add_marks=True):
@@ -80,15 +85,16 @@ class LineFitHandler:
             self.lines.append(line)
             self.slopes.append(slope)
         except (LinAlgError, SystemError) as e:
+            print(e)
             pass
 
     def _fit_to_traces(self):
         self._clear_lines()
         for trace in self.figure.data:
-            self._fit_to_trace(trace)
+            self._fit_to_trace(trace, add_marks=True)
 
     def _clear_lines(self):
-        self.figure.data = [mark for mark in self.figure.data if mark not in self.line_marks]
+        self.figure.data = [mark for mark in self.figure.data if mark not in self.lines]
         self.lines = []
         self.slopes = []
 
